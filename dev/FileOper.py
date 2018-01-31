3d71432fb8ff4db511fbab6240a7d2a932c77bc5
k0305916 <k03059164@sina.com>
2018-01-31 18:45:23 +0800

    修改车辆数据结构。

--- dev/FileOper.py
+++ dev/FileOper.py
@@ -3,31 +3,14 @@ import os
 
 class Vehicle:
     '车辆结构数据'
-    __car_number = ""
-    __trip_id = ""
-    __datetime_record = ""
-    __longitude = ""
-    __latitude = ""
-    __speed = ""
-    __direction = ""
-    __region = ""
-
-    def __init__(self, car_number, trip_id, datetime_record, longitude, latitude, speed, direction):
-        self.__car_number = car_number
-        self.__trip_id = trip_id
-        self.__datetime_record = datetime_record
-        self.__longitude = longitude
-        self.__latitude = latitude
-        self.__speed = speed
-        self.__direction = direction
-
-    def setregion(self, region):
-        '设置区域'
-        self.__region = region
-
-    def getvalue(self):
-        '返回数据'
-        return [self.__car_number, self.__trip_id, self.__datetime_record, self.__longitude, self.__latitude, self.__speed, self.__direction, self.__region]
+    car_number = ""
+    trip_id = ""
+    datetime_record = ""
+    longitude = ""
+    latitude = ""
+    speed = ""
+    direction = ""
+    region = ""
 
 
 class FileOper:
@@ -55,19 +38,30 @@ class FileOper:
                 continue
 
             value = eachline.split(',')
-            vehicle = Vehicle(value[0], value[1], value[2],
-                              value[3], value[4], value[5], value[6])
+            vehicle = Vehicle()
+            vehicle.car_number = value[0]
+            vehicle.trip_id = value[1]
+            vehicle.datetime_record = value[2]
+            vehicle.longitude = value[3]
+            vehicle.latitude = value[4]
+            vehicle.speed = value[5]
+            vehicle.direction = value[6]
             #通过arcgis+loction得到区域值
             region = "0"
 
-            vehicle.setregion(region)
+            vehicle.region = region
             self.__data.append(vehicle)
 
     def outputfile(self):
         '输出文件'
         for data in self.__data:
-            value = data.getvalue
-            self.__outfile.writelines(value[0]+','+value[1]+','+value[2]+','+value[3] +
-                                      ','+value[4]+','+value[5]+','+value[6]+','+value[7]+','+value[8])
+            self.__outfile.writelines(data.car_number+',' +
+                                      data.trip_id+',' +
+                                      data.datetime_record+',' +
+                                      data.longitude+',' +
+                                      data.latitude+',' +
+                                      data.speed+',' +
+                                      data.direction+',' +
+                                      data.region)
 
         self.__outfile.close()
