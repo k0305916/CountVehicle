9a671e6d7b6ad9d670614b0fcfbd858289b87f22
k0305916 <k03059164@sina.com>
2018-02-01 20:27:54 +0800

    添加空车计算模块。

--- dev/Main.py
+++ dev/Main.py
@@ -4,43 +4,50 @@ from multiprocessing import cpu_count
 from concurrent.futures import ThreadPoolExecutor, wait
 from FileOper import FileOper
 from FileConvert import FileConvert
+from FileCount import FileCount
 
 FILELIST = []
-FILELISTCONVERT = []
+FILELISTCONVERT = [['E:\\Business\\Python\\CountVehicle\\CountVehicle\\data\\112151.csv']]
 
 
 def main():
     """
     input fie path
     """
-    if len(sys.argv) <= 2:
-        print("请输入正确的参数：文件夹路径 shp文件夹路径")
-        return
+    # if len(sys.argv) <= 2:
+    #     print("请输入正确的参数：文件夹路径 shp文件夹路径")
+    #     return
 
-    path = sys.argv[1]
-    if os.path.isdir(path) != True:
-        print("请输入正确的文件夹路径.")
-        return
+    # path = sys.argv[1]
+    # if os.path.isdir(path) != True:
+    #     print("请输入正确的文件夹路径.")
+    #     return
 
-    #获取该路径下的所有以.csv结尾的文件
-    csvfiles = [os.path.join(r, file) for r, d, files in os.walk(
-        path) for file in files if file.endswith(".csv")]
-    cpucount = cpu_count()
+    # #获取该路径下的所有以.csv结尾的文件
+    # csvfiles = [os.path.join(r, file) for r, d, files in os.walk(
+    #     path) for file in files if file.endswith(".csv")]
+    # cpucount = cpu_count()
 
-    #引入线程池进行多线程操作
+    # #引入线程池进行多线程操作
     executor = ThreadPoolExecutor(1)
     futures = []
-    for csv in csvfiles:
-        futures.append(executor.submit(funccutfile, csv))
-    wait(futures)
-    futures.clear()
-    print("切分文件完成，开始进行文件转换.....")
+    # for csv in csvfiles:
+    #     futures.append(executor.submit(funccutfile, csv))
+    # wait(futures)
+    # futures.clear()
+    # print("切分文件完成，开始进行文件转换.....")
     # for filelist in FILELIST:
     #     for file in filelist:
     #         futures.append(executor.submit(fucconvertfile, file))
     # wait(futures)
+    # futures.clear()
     # print("文件转换完成，开始计算空车数量.....")
-
+    for filelist in FILELISTCONVERT:
+        for file in filelist:
+            futures.append(executor.submit(funccountfile, file))
+    wait(futures)
+    futures.clear()
+    print("空车计算完成，请检查。")
 
 def funccutfile(filepath):
     '生成文件操作对象，并处理'
@@ -54,6 +61,12 @@ def fucconvertfile(filepath):
     '转换文件'
     file = FileConvert(filepath)
     file.convert()
+
+def funccountfile(filepath):
+    '计算空车数量'
+    file = FileCount(filepath)
+    file.run()
+    file.outputfile()
 
 
 if __name__ == "__main__":
