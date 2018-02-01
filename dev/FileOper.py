6ed60a3fa1f77d191329b8255f8640000f2b1c7a
k0305916 <k03059164@sina.com>
2018-02-01 17:21:12 +0800

    根据tripid切分文件。

--- dev/FileOper.py
+++ dev/FileOper.py
@@ -15,22 +15,18 @@ class Vehicle:
 
 class FileOper:
     '处理文件的类'
-    #额。。。不知道这样写是否正确。。。。。
-    __outfile = open()
-    __data = []
+    __data = {}
+    __filepath = ""
+    __filelist = []
 
     def __init__(self, filepath):
-        path, file = os.path.split(filepath)
-        name, extention = os.path.splitext(file)
-        self.outfile = open(path+name+"_convert"+extention, 'wt')
-        self.outfile.writelines(
-            'car_number,trip_id,datetime_record,longitude,latitude,speed,direction,region')
+        self.__filepath = filepath
 
-    def processfile(self, filepath):
+    def cutfile(self):
         """
         处理文件：取出每行的数据，将其与arcgis的区域API结合，得到匹配区域。
         """
-        f_read = open(filepath)
+        f_read = open(self.__filepath)
         fristline = False
         for eachline in f_read:
             if not fristline:
@@ -46,22 +42,34 @@ class FileOper:
             vehicle.latitude = value[4]
             vehicle.speed = value[5]
             vehicle.direction = value[6]
-            #通过arcgis+loction得到区域值
-            region = "0"
 
-            vehicle.region = region
-            self.__data.append(vehicle)
+            if vehicle.trip_id in self.__data:
+                self.__data[vehicle.trip_id].append(vehicle)
+            else:
+                self.__data[vehicle.trip_id] = [vehicle]
 
     def outputfile(self):
         '输出文件'
-        for data in self.__data:
-            self.__outfile.writelines(data.car_number+',' +
-                                      data.trip_id+',' +
-                                      data.datetime_record+',' +
-                                      data.longitude+',' +
-                                      data.latitude+',' +
-                                      data.speed+',' +
-                                      data.direction+',' +
-                                      data.region)
+        path, file = os.path.split(self.__filepath)
+        name, extention = os.path.splitext(file)
+        if not os.path.exists(path+'\\'+name):
+            os.mkdir(path+'\\'+name)
+        for _data in self.__data:
+            outfile = open(path+'\\'+name+'\\'+_data+extention, 'wt')
+            outfile.writelines(
+                'car_number,trip_id,datetime_record,longitude,latitude,speed,direction\n')
+            outfile.flush()
+            self.__filelist.append(path+'\\'+name+'\\'+_data+extention)
+            for data in self.__data[_data]:
+                outfile.writelines(data.car_number+',' +
+                                   data.trip_id+',' +
+                                   data.datetime_record+',' +
+                                   data.longitude+',' +
+                                   data.latitude+',' +
+                                   data.speed+',' +
+                                   data.direction)
+                outfile.flush()
+            outfile.close()
 
-        self.__outfile.close()
+    def getfilelist(self):
+        return self.__filelist
